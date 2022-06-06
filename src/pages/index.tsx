f744ea3c2d71b8fdabd5fc744fbd4b2691294e8c
Felipe Moraes <felipe.omoraes@outlook.com>
2022-06-06 09:28:18 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,38 @@
+import { Flex, Button, Stack } from '@chakra-ui/react'
+
+import { Input } from '../components/Form/Input';
+
+export default function SignIn() {
+  return (
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
+      <Flex
+        as="form"
+        w="100%"
+        maxW="360px"
+        bg="gray.800"
+        p="8"
+        borderRadius="8px"
+        direction="column"
+      >
+        <Stack spacing="4">
+          <Input name="email" label="E-mail" type="email" />
+          <Input name="passwprd" label="Senha" type="password" />
+        </Stack>
+        <Button
+            type="submit"
+            mt="6"
+            colorScheme="pink"
+            size="lg"
+        >
+          Entrar
+        </Button>
+
+      </Flex>
+    </Flex>
+  )
+}
